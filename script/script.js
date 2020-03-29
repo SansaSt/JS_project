2e39b946f0c9e882eb504cd835a2ebda6630cd83
SansaSt <sc.comsa@mail.ru>
2020-03-29 15:02:38 +0500

    lesson02

--- script/script.js
+++ script/script.js
@@ -3,6 +3,29 @@ let money,
     addExpenses,
     deposit,
     mission,
-    period;
-alert(Привет, Андрей);
-console.log('Урок первый');
+    period,
+    budgetDay;
+
+money = 50000;
+income = 'Вклад';
+addExpenses = 'Еда, бензин, квартплата';
+deposit = true;
+mission = 300000;
+period = 10;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+
+budgetDay = money / 30;
+console.log(budgetDay);
+
+
+
+
+
+
