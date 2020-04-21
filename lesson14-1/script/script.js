41bcdd748885282bba2386d98c7dac57fe8e20d3
SansaSt <sc.comsa@mail.ru>
2020-04-21 17:31:52 +0500

    lesson14-1

---
+++ lesson14-1/script/script.js
@@ -0,0 +1,40 @@
+const body = document.querySelector('body');
+
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+};
+
+DomElement.prototype.render = function() { 
+
+  let newElement;
+  if (this.selector[0] === '.') {
+    newElement = document.createElement('div');
+    newElement.classList.add('block');
+  } else if (this.selector[0] === '#') {
+    newElement = document.createElement('p');
+    newElement.classList.add('best');
+  } 
+
+  newElement.style.cssText = 'height: this.height'; 
+  newElement.style.cssText = 'width: this.width';
+  newElement.style.cssText = 'background-color: this.bg';
+  newElement.style.cssText = 'font-size: this.fontSize';
+  newElement.textContent = 'Hello, world';
+
+  body.append(newElement);
+  console.log('newElement: ', newElement);
+
+};
+
+let newItem1 = new DomElement('.div', '20px', '10px', '#ffffff', '14px');
+newItem1.render();
+
+let newElem2 = new DomElement('#id', '20px', '10px', 'red', '14px');
+newElem2.render();
+
+
+
